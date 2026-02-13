64ddfe6e91077f61d608d14e9c94b697a9284606
Bhanuprakash5b0 <palabindelabhanuprakash@gmail.com>
2026-02-13 12:35:47 +0530

    making port changes

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 from flask_caching import Cache
 from routes.quiz_routes import quiz_bp
 from init_cache import cache
+import os
 
 def create_app():
     app = Flask(__name__)
@@ -19,4 +20,6 @@ def create_app():
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    # host='0.0.0.0' is the critical fix here
+    app.run(host='0.0.0.0', port=port)
